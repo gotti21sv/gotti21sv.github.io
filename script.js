4aa9b1ad66443dacc8a6174f8486c9b2955fa1db
gotti21sv <kdavaj90@gmail.com>
2026-06-29 10:04:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,43 +3,43 @@ const products = [
 {
 name:"Black Hoodie",
 price:"1700 грн",
-image:8C130B78-
-B7E7-443F-9AE8-439ABEDD649B.png
+image:"8C130B78-
+B7E7-443F-9AE8-439ABEDD649B.png"
 },
 
 {
 name:"18+ Hoodie",
 price:"2000 грн",
-image:BDD2C093-6CAB-46D1-
-B3CE-4CC5F699F30C.png
+image:"BDD2C093-6CAB-46D1-
+B3CE-4CC5F699F30C.png"
 },
 
 {
 name:"Pink Hoodie",
 price:"1800 грн",
-image:F62943A9-9AEB-4CDF-
-BA27-1C504C1FDCC8.png
+image:"F62943A9-9AEB-4CDF-
+BA27-1C504C1FDCC8.png"
 },
 
 {
 name:"Cross T-Shirt",
 price:"1000 грн",
-image:261898D7-7E97-45C3-
-A30E-37091EA36423.png
+image:"261898D7-7E97-45C3-
+A30E-37091EA36423.png"
 },
 
 {
 name:"Black Shorts",
 price:"1400 грн",
-image:F0OB8C5C-OADE-494C-869D-
-DAE50880EE70.png
+image:"F0OB8C5C-OADE-494C-869D-
+DAE50880EE70.png"
 },
 
 {
 name:"18+ Longsleeve",
 price:"1300 грн",
-image:FAB8E75D-6289-4ED5-BA9B-
-A2CBF6FB108C.png
+image:"FAB8E75D-6289-4ED5-BA9B-
+A2CBF6FB108C.png"
 }
 
 ];
